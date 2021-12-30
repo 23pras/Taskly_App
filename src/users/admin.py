46b959524e55c4687fd168113343f1bab8c81d12
23pras <palprashant465@gmail.com>
2021-12-31 02:01:02 +0530

    comment added

--- src/users/admin.py
+++ src/users/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
+    readonly_fields = ('id',)                   #id of every user will be displayed in read-only format.
 
 admin.site.register(Profile,ProfileAdmin)
--- src/users/models.py
+++ src/users/models.py
@@ -12,19 +12,19 @@ class GenerateProfileImagePath(object):
         pass
     
     def __call__(self,instance,filename):
-        ext = filename.split('.')[-1]
-        path = f'media/accounts/{instance.user.id}/images/'
-        name = f'profile_image.{ext}'
-        return os.path.join(path, name)
+        ext = filename.split('.')[-1]                             #taking the extension of uploaded image file.
+        path = f'media/accounts/{instance.user.id}/images/'       #instance here used as a reference for profiles.
+        name = f'profile_image.{ext}'                          # extension extracted and stored into another variable with the help of string interpolation.
+        return os.path.join(path, name)                #adding the patha and extracted name here.
 
 user_profile_image_path = GenerateProfileImagePath()
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to=user_profile_image_path, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)               # models.CASACADE will deleted the profile with user.
+    image = models.FileField(upload_to=user_profile_image_path, blank=True, null=True)  #inside upload we have defind path to store image.
 
     def __str__(self):
-        return  f'{self.user.username}\'s Profile'
+        return  f'{self.user.username}\'s Profile'                   #used it so that user name can be displayed instead of just profile object.
 
 
